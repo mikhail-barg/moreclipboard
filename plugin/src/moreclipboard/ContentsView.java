ff2e51d7e6bd0b444a0716853cb787f1f85a2ee8
mikhail.barg@gmail.com <mikhail.barg@gmail.com>
2016-06-06 19:46:04 +0300

    This is a proper way to do enablement state for Commands/Handlers, but it does not work in E4.

--- plugin/src/moreclipboard/ContentsView.java
+++ plugin/src/moreclipboard/ContentsView.java
@@ -4,6 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandler2;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
@@ -22,6 +23,7 @@ import org.eclipse.ui.contexts.IContextActivation;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.handlers.IHandlerService;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.services.IEvaluationService;
 
 /**
  * A clipboard contents View
@@ -29,93 +31,12 @@ import org.eclipse.ui.part.ViewPart;
  */
 public class ContentsView extends ViewPart implements SelectionListener
 {
-	/*
-	////////////////////////////////////////////////////////////////////
-	class RemoveCurrentItemAction extends Action
-	{
-		RemoveCurrentItemAction()
-		{
-			super(Messages.ContentsView_RemoveCurrentActionName);
-
-			setImageDescriptor(Plugin.getImage("/icons/enabl/rem.gif")); //$NON-NLS-1$
-			setDisabledImageDescriptor(Plugin.getImage("/icons/disabl/rem.gif")); //$NON-NLS-1$
-			setActionDefinitionId("MoreClipboard.commands.ContentsView.deleteCurrent");  //$NON-NLS-1$
-		}
-
-		@Override
-		public void run()
-		{
-			removeCurrentItem();
-		}
-	}
-	////////////////////////////////////////////////////////////////////	
-	static class ClearContentsAction extends Action
-	{
-		public ClearContentsAction()
-		{
-			super(Messages.ContentsView_RemoveAllActionName);
-
-			setImageDescriptor(Plugin.getImage("/icons/enabl/remall.gif")); //$NON-NLS-1$
-			setDisabledImageDescriptor(Plugin.getImage("/icons/disabl/remall.gif")); //$NON-NLS-1$
-		}
-
-		@Override
-		public void run()
-		{
-			Plugin.getInstance().getContents().clear();
-		}
-	}
-	////////////////////////////////////////////////////////////////////
-	class MoveCurrentElementUpAction extends Action
-	{
-		MoveCurrentElementUpAction()
-		{
-			super(Messages.ContentsView_MoveUp);
-
-			setImageDescriptor(Plugin.getImage("/icons/enabl/up.gif")); //$NON-NLS-1$
-			setDisabledImageDescriptor(Plugin.getImage("/icons/disabl/up.gif")); //$NON-NLS-1$
-		}
-
-		@Override
-		public void run()
-		{
-			moveCurrentElementUp();
-		}
-	}
-	////////////////////////////////////////////////////////////////////
-	class MoveCurrentElementDownAction extends Action
-	{
-		MoveCurrentElementDownAction()
-		{
-			super(Messages.ContentsView_MoveDown);
-
-			setImageDescriptor(Plugin.getImage("/icons/enabl/down.gif")); //$NON-NLS-1$
-			setDisabledImageDescriptor(Plugin.getImage("/icons/disabl/down.gif")); //$NON-NLS-1$
-		}
-
-		@Override
-		public void run()
-		{
-			moveCurrentElementDown();
-		}
-	}	
-	///////////////////////////////////////////////////////////////////////////////////////
-	
-	*/
-	
-	
 	private org.eclipse.swt.widgets.List m_listView;
-	/*
-	private Action m_removeCurAction;
-	private Action m_removeAllAction;
-	private Action m_moveCurUpAction;
-	private Action m_moveCurDownAction;
-	*/
 	private IContextActivation m_contextActivation;
-	private IHandler m_removeCurHandler;
-	private IHandler m_removeAllHandler;
-	private IHandler m_moveUpHandler;
-	private IHandler m_moveDownHandler;
+	private IHandler2 m_removeCurHandler;
+	private IHandler2 m_removeAllHandler;
+	private IHandler2 m_moveUpHandler;
+	private IHandler2 m_moveDownHandler;
 
 	@Override
 	public void createPartControl(Composite parent)
@@ -135,9 +56,9 @@ public class ContentsView extends ViewPart implements SelectionListener
 				return null;
 			}
 			@Override
-			public boolean isEnabled() 
+			public void setEnabled(Object evaluationContext) 
 			{
-				return m_listView.getSelectionIndex() >= 0;
+				setBaseEnabled(m_listView.getSelectionIndex() >= 0);
 			}
 		};
 		m_removeAllHandler = new AbstractHandler() {
@@ -148,9 +69,9 @@ public class ContentsView extends ViewPart implements SelectionListener
 				return null;
 			}
 			@Override
-			public boolean isEnabled() 
+			public void setEnabled(Object evaluationContext) 
 			{
-				return m_listView.getItemCount() > 0;
+				setBaseEnabled(m_listView.getItemCount() > 0);
 			}
 		};
 		m_moveUpHandler = new AbstractHandler() {
@@ -161,9 +82,9 @@ public class ContentsView extends ViewPart implements SelectionListener
 				return null;
 			}
 			@Override
-			public boolean isEnabled() 
+			public void setEnabled(Object evaluationContext)
 			{
-				return m_listView.getSelectionIndex() >= 1;
+				setBaseEnabled(m_listView.getSelectionIndex() >= 1);
 			}
 		};
 		m_moveDownHandler = new AbstractHandler() {
@@ -174,24 +95,16 @@ public class ContentsView extends ViewPart implements SelectionListener
 				return null;
 			}
 			@Override
-			public boolean isEnabled() 
+			public void setEnabled(Object evaluationContext) 
 			{
-				return m_listView.getSelectionIndex() < m_listView.getItemCount() - 1;
+				setBaseEnabled(m_listView.getSelectionIndex() < m_listView.getItemCount() - 1);
 			}
 		};
-		
-		
 		handlerService.activateHandler("MoreClipboard.commands.ContentsView.deleteCurrent", m_removeCurHandler); 
 		handlerService.activateHandler("MoreClipboard.commands.ContentsView.deleteAll", m_removeAllHandler);
 		handlerService.activateHandler("MoreClipboard.commands.ContentsView.moveUp", m_moveUpHandler);
 		handlerService.activateHandler("MoreClipboard.commands.ContentsView.moveDown", m_moveDownHandler);
 		
-		/*
-		createActions();
-		createContextMenu();
-		initToolBar();
-		*/
-
 		MenuManager manager = new MenuManager();
 		Menu contextMenu = manager.createContextMenu(m_listView);
 		m_listView.setMenu(contextMenu);
@@ -199,58 +112,26 @@ public class ContentsView extends ViewPart implements SelectionListener
 		
 		Contents contents = Plugin.getInstance().getContents();
 		contents.registerView(this);
+		
+		updateHandlersEnabledState();
 	}
 	
-	/*
-	private void createActions()
-	{
-		m_removeCurAction = new RemoveCurrentItemAction();
-		m_removeAllAction = new ClearContentsAction();
-		m_moveCurUpAction = new MoveCurrentElementUpAction();
-		m_moveCurDownAction = new MoveCurrentElementDownAction();
-		
-		
-		
-		updateActionsEnabledState();
-		
-	}
-	
-	private void createContextMenu()
-	{
-		MenuManager manager = new MenuManager();
-		manager.setRemoveAllWhenShown(true);
-		manager.addMenuListener(new IMenuListener()
-			{
-				@Override
-				public void menuAboutToShow(IMenuManager manager)
-				{
-					manager.add(m_moveCurUpAction);
-					manager.add(m_moveCurDownAction);
-					manager.add(m_removeCurAction);
-					manager.add(m_removeAllAction);
-				}
-			});
-		m_listView.setMenu(manager.createContextMenu(m_listView));
-	}
-
-	private void initToolBar()
-	{
-		IToolBarManager tm = getViewSite().getActionBars().getToolBarManager();
-		tm.add(m_moveCurUpAction);
-		tm.add(m_moveCurDownAction);
-		tm.add(m_removeCurAction);
-		tm.add(m_removeAllAction);
-	}
-
-	private void updateActionsEnabledState()
+	private void updateHandlersEnabledState()
 	{
 		boolean hasItems = m_listView.getItemCount() > 0;
+		/*
 		m_removeCurAction.setEnabled(m_listView.getSelectionIndex() >= 0);
 		m_removeAllAction.setEnabled(hasItems);
 		m_moveCurUpAction.setEnabled(hasItems && m_listView.getSelectionIndex() >= 1);
 		m_moveCurDownAction.setEnabled(hasItems && m_listView.getSelectionIndex() < m_listView.getItemCount() - 1);
+		*/
+		//IEvaluationService evaluationService = (IEvaluationService)getSite().getService(IEvaluationService.class);
+		//evaluationService.requestEvaluation(propertyName);
+		m_removeCurHandler.setEnabled(null);
+		m_removeAllHandler.setEnabled(null);
+		m_moveUpHandler.setEnabled(null);
+		m_moveDownHandler.setEnabled(null);
 	}
-	*/
 	
 	
 	/**
@@ -286,7 +167,7 @@ public class ContentsView extends ViewPart implements SelectionListener
 		}
 		
 		m_listView.setItems(preparedElements);
-		//updateActionsEnabledState();
+		updateHandlersEnabledState();
 	}
 
 	@Override
@@ -308,7 +189,7 @@ public class ContentsView extends ViewPart implements SelectionListener
 	@Override
 	public void widgetSelected(SelectionEvent e)
 	{
-		//updateActionsEnabledState();
+		updateHandlersEnabledState();
 	}
 
 	private void removeCurrentItem()
@@ -328,7 +209,7 @@ public class ContentsView extends ViewPart implements SelectionListener
 		if (itemIndex >= 0)
 		{
 			m_listView.setSelection(itemIndex);
-			//updateActionsEnabledState();
+			updateHandlersEnabledState();
 		}
 	}
 	
@@ -343,7 +224,7 @@ public class ContentsView extends ViewPart implements SelectionListener
 		
 		//keep same item selected and update actions state
 		m_listView.setSelection(itemIndex - 1);
-		//updateActionsEnabledState();
+		updateHandlersEnabledState();
 	}
 	
 	private void moveCurrentElementDown()
@@ -357,7 +238,7 @@ public class ContentsView extends ViewPart implements SelectionListener
 		
 		//keep same item selected and update actions state
 		m_listView.setSelection(itemIndex + 1);
-		//updateActionsEnabledState();
+		updateHandlersEnabledState();
 	}
 }
 
